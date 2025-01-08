f1db7d55ba5a4ffe3a005cda0235f84a01dd23e9
vigneshv1cky <97196163+vigneshv1cky@users.noreply.github.com>
2025-01-08 21:01:38 +0530

    Added Evi and Weather plots

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import random
+
+# Set global plot style and parameters
+plt.style.use("ggplot")
+plt.rcParams["figure.figsize"] = [20, 5]
+plt.rcParams["figure.dpi"] = 100
 
 dataset = "../data/raw/Datasets/Challenge-1/C1 - Tallgrass-Prairie.xlsx"
 
@@ -47,3 +53,28 @@ p_20 = pd.read_excel(
     date_parser=lambda x: pd.to_datetime(x, format="%m/%d/%Y"),
 )
 p_20.name = "P20"
+
+
+weather = pd.read_excel(dataset, sheet_name="Weather data")
+
+# Create a datetime column from the year, month, and day columns
+weather["Date"] = pd.to_datetime(
+    {
+        "year": weather["YEAR"],
+        "month": weather["MONTH"],
+        "day": weather["DAY"],
+    }
+)
+
+# Convert the Date column to strings in "month-day-year" format
+weather["Date"] = weather["Date"].dt.strftime("%m-%d-%Y")
+# Convert 'Date' back to datetime if it's currently a string
+weather["Date"] = pd.to_datetime(weather["Date"])
+
+p_13.to_pickle("../data/processed/p_13.pkl")
+p_14.to_pickle("../data/processed/p_14.pkl")
+p_15.to_pickle("../data/processed/p_15.pkl")
+p_16.to_pickle("../data/processed/p_16.pkl")
+p_18.to_pickle("../data/processed/p_18.pkl")
+p_20.to_pickle("../data/processed/p_20.pkl")
+weather.to_pickle("../data/processed/weather.pkl")
